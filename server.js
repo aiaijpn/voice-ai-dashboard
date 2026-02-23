fa2f5aa2bda626e788f7fbb7fdfbb22017bbb361
aiaijpn <takamura.corefine@gmail.com>
2026-02-23 15:13:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ app.post("/webhook", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  //console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
