a13cc953a0ac4bdcbd68821dc3e97b723a419d99
Eiichi Takamura <takamura.corefine@gmail.com>
2026-03-08 13:41:34 +0900

    fix: ads id重複修正と広告ニュアンス調整

--- ads/ads.js
+++ ads/ads.js
@@ -2,9 +2,9 @@
 
 module.exports = [
   { id: "ad1", title: "ワインバー小澤", weight: 1 },
-  { id: "ad2", title: "三味線ファンクラブ", weight: 5 },
+  { id: "ad2", title: "三味線ファンクラブ", weight: 7 },
   { id: "ad3", title: "池田法律相談室", weight: 2 },
-  { id: "ad3", title: "相続対策なら尾形", weight: 1 },
-  { id: "ad3", title: "美容医療の西川ヘレン", weight: 2 },
-  { id: "ad4", title: "オーダースーツ金井", weight: 1 }
+  { id: "ad4", title: "相続対策なら尾形", weight: 1 },
+  { id: "ad5", title: "美容医療の西川ヘレン", weight: 2 },
+  { id: "ad6", title: "オーダースーツ金井", weight: 1 }
 ];
